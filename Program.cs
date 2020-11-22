7fe7b2c283a6c31891d62a8d98645d7c3a66b254
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-11-22 18:09:17 -0300

    Add comentarios

--- Program.cs
+++ Program.cs
@@ -14,12 +14,11 @@ namespace Media_de_alunos
             float[] media = new float[10];
             string[] totalaprovados = new string[10];
             string[] totalreprovados = new string[10];
-            
-            
             int ap, rp;
-            //int count, count2;
+
 
             Console.WriteLine("Média dos alunos\n----------------");
+            //Coletando Info
             for (var contador = 0; contador < 4; contador++)
             {
                 Console.WriteLine("digite o nome do(a) aluno(a)");
@@ -53,6 +52,9 @@ namespace Media_de_alunos
                 }
             }
             Console.WriteLine("Alunos cadastrados!\n");
+            //Fim do coletando infos
+
+            //Exibindo Alunos e notas
             for (var contador = 0; contador < 4; contador++)
             {
                 Console.WriteLine(nomes[contador]);
@@ -62,15 +64,20 @@ namespace Media_de_alunos
                 Console.WriteLine(nota4[contador]);
                 Console.WriteLine("-----------------------");
             }
+            //Fim exibindo alunos e notas
+
+            //Mostrando media individual
             Console.WriteLine($"Média\n------------------");
             ap = 0;
             rp = 0;
+
+            //Calculando a média
             for (var contador = 0; contador < 4; contador++)
             {
-                
                 media[contador] = (nota1[contador] + nota2[contador] + nota3[contador] + nota4[contador]) / 4;
                 Console.WriteLine(nomes[contador]);
                 Console.WriteLine("Média: "+notas(nota1[contador], nota2[contador], nota3[contador], nota4[contador]));
+                //Verificando reprovação
                 if (media[contador] >= 6 && media[contador] <= 10)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -89,16 +96,33 @@ namespace Media_de_alunos
                     rp = rp + 1;
                     totalreprovados[contador] = nomes[contador];
                 }
+                //fim verificando reprovação
             }
+            //Fim calculando a média
+
+            //Alunos aprovados
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Alunos aprovados");
+            Console.ResetColor();
+            Console.WriteLine("-----------------");
+
             string[] apvs = new string[ap];
             
             for (var contador = 0; contador < ap; contador++)
             {
                 apvs[contador] = totalaprovados[contador];
             }
+            
+            for (var contador = 0; contador < ap; contador++)
+            {
+                Console.WriteLine("-"+apvs[contador]);
+            }
+            Console.WriteLine("-----------------");
+            //Fim alunos aprovados
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Alunos aprovados");
+            //Alunos reprovados
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Alunos reprovados");
             Console.ResetColor();
             Console.WriteLine("-----------------");
 
@@ -109,37 +133,31 @@ namespace Media_de_alunos
                 rpvs[contador] = totalreprovados[contador];
             }
 
-            for (var contador = 0; contador < ap; contador++)
-            {
-                Console.WriteLine("-"+apvs[contador]);
-            }
-            Console.WriteLine("-----------------");
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Alunos reprovados");
-            Console.ResetColor();
-            Console.WriteLine("-----------------");
-
             for (var contador = 0; contador < rp; contador++)
             {
                 Console.WriteLine("-"+rpvs[contador]);
             }
+            //Fim alunos reprovados
 
-
-
+            //Total de alunos aprovados ou não
             Console.WriteLine("-----------------");
             Console.WriteLine($"Total de alunos aprovados: {ap}");
             Console.WriteLine($"Total de alunos reprovados: {rp}");
+            //Total de alunos aprovados ou não
 
+            //Media geral da sala
             Console.WriteLine("-----------------");
             Console.WriteLine("Média geral da sala: "+medias(media[0], media[1], media[2], media[3], media[4], media[5], media[6], media[7], media[8], media[9]));
             Console.WriteLine("-----------------");
+            //Media geral da sala
 
-
+            //Calculando media individual
             static float notas(float a, float b, float c, float d){
                 float resu = (a + b + c + d) / 4;
                 return resu;
             }
+            
+            //Calculando media geral
             static float medias(float a, float b, float c, float d, float e, float f, float g, float h, float i, float j){
                 float resumed = (a + b + c + d + e + f + g + h + i + j) / 4;
 
